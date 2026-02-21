5507eb84fb3d0b18fa9113669c5354597a7fc73e
DineshSethu001 <dineshsethu15981@gmail.com>
2026-02-21 22:55:06 +0530

    removed

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,10 +3,6 @@
 import "./globals.css";
 import Providers from "./providers";
 
-export const metadata = {
-  title: "Dcode Portfolio",
-  description: "It is the website that showcase my skills",
-};
 
 export default function RootLayout({ children }) {
   return (
