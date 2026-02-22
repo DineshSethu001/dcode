9e498415bfca6045b9f0ff2540d334cc6481a440
DineshSethu001 <dineshsethu15981@gmail.com>
2026-02-22 16:46:12 +0530

    F

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import NavbarWrapper from "./components/NavbarWrapper";
 import { About, Services, Projects, Contact, Footer } from "./components";
 
-import { Services} from "./components/Experience.jsx";
+import { Experience} from "./components/Experience.jsx";
 import { Header } from "./components/Header.jsx";
 
 export default function Home() {
